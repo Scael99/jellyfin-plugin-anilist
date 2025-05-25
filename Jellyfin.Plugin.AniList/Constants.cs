4b25e14631f49418014cfb3e3582629707189d1a
Scael99 <110110027+Scael99@users.noreply.github.com>
2025-05-25 06:31:10 +0000

    modified constants.cs

--- Jellyfin.Plugin.AniList/Constants.cs
+++ Jellyfin.Plugin.AniList/Constants.cs
@@ -2,7 +2,7 @@ namespace Jellyfin.Plugin.AniList
 {
     public static class Constants
     {
-        public const string PluginName = "AniList";
-        public const string PluginGuid = "1bd22884-44be-40f3-ad95-fc4a7834ba2c";
+        public const string PluginName = "AniList English";
+        public const string PluginGuid = "fe9c23f6-1eb9-4bfb-b7ca-4557ca2707e3";
     }
 }
